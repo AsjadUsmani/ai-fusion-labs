6616d6274a0da0f6cbc99b71a50e9358fe538221
Asjad Usmani <asjadusmani4@gmail.com>
2025-11-01 12:57:11 +0530

    Added Layout and Multimodel

--- app/Provider.jsx
+++ app/Provider.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
 import { AppSidebar } from './_components/AppSidebar'
+import AppHeader from './_components/AppHeader'
 
 const Provider = ({
     children,
@@ -15,9 +16,16 @@ const Provider = ({
             disableTransitionOnChange
             {...props}>
             <SidebarProvider>
-                <AppSidebar/>
-                <SidebarTrigger />
-                {children}
+                <div className="flex w-full">
+                    {/* Sidebar on the left */}
+                    <AppSidebar />
+
+                    {/* Main content on the right */}
+                    <div className="flex flex-col w-full">
+                        <AppHeader />
+                        {children}
+                    </div>
+                </div>
             </SidebarProvider>
         </NextThemesProvider>
     )
